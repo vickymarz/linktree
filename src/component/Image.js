5e417db47c390370de18ef598c561c1a78793cd6
vickymarz <okorojiebube2@gmail.com>
2022-10-31 13:34:20 +0100

    Add homepage component

---
+++ src/component/Image.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+const Image = (src, alt, className) => {
+  return (
+    <img src={src} alt={alt} className={className} />
+  )
+}
+
+Image.propTypes = {
+    className: PropTypes.string,
+    src: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
+  };
+
+Image.defaultProps = {
+    className: '',
+  };
+  
+export default Image
---
+++ src/component/LInks.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LInks = () => {
+  return (
+    <div>
+        <Link to={link}>
+            {children}
+        </Link>
+    </div>
+  )
+}
+
+export default LInks
