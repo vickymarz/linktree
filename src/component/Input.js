38ca8bd5e2fbdfa756e7d24ded203d0f5f20418f
vickymarz <okorojiebube2@gmail.com>
2022-11-05 14:23:02 +0100

    Add validation

--- src/component/Input.js
+++ src/component/Input.js
@@ -2,29 +2,47 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 const Input = ({
-  type, name, placeholder, className, checked, onChange, id,
+  type, name, text, placeholder, className, checked, value, onChange, id, innerRef, errorMessage,
 }) => (
   <div className={className}>
-    <label htmlFor={id}>{name}</label>
-    <input type={type} id={id} checked={checked} placeholder={placeholder} onChange={onChange} />
+    <label htmlFor={id}>{text}</label>
+    <input
+      type={type}
+      id={id}
+      name={name}
+      value={value}
+      checked={checked}
+      placeholder={placeholder}
+      onChange={onChange}
+      ref={innerRef}
+    />
+    <small>{errorMessage}</small>
   </div>
 );
 
 Input.propTypes = {
   type: propTypes.string.isRequired,
-  name: propTypes.string.isRequired,
+  text: propTypes.string.isRequired,
+  name: propTypes.string,
   placeholder: propTypes.string,
   className: propTypes.string,
+  errorMessage: propTypes.string,
   id: propTypes.string.isRequired,
   checked: propTypes.bool,
   onChange: propTypes.func,
+  innerRef: propTypes.oneOfType([propTypes.object]),
+  value: propTypes.string,
 };
 
 Input.defaultProps = {
   placeholder: '',
   className: '',
-  checked: '',
-  onChange: '',
+  checked: false,
+  value: '',
+  name: '',
+  errorMessage: '',
+  onChange: null,
+  innerRef: null,
 };
 
 export default Input;
--- src/component/pages/Contact.js
+++ src/component/pages/Contact.js
@@ -1,13 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styles from '../../css/contact.module.css';
 import Footer from '../Footer';
 import Input from '../Input';
 
 const Contact = () => {
   const [isChecked, setIsChecked] = useState(false);
+  const [errors, setErrors] = useState({});
+
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+  const emailRef = useRef(null);
+  const textAreaRef = useRef(null);
 
   const handleCheckbox = () => {
     setIsChecked(!isChecked);
+  };
+
+  const validate = () => {
+    const errors = {};
+
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+    if (firstNameRef.current.value.length === 0 || firstNameRef.current.value.trim() === '') {
+      errors.first_name = 'Please enter a message';
+      firstNameRef.current.focus();
+    }
+
+    if (lastNameRef.current.value.length === 0 || lastNameRef.current.value.trim() === '') {
+      errors.last_name = 'Please enter a message';
+      lastNameRef.current.focus();
+    }
+
+    if (!regex.test(emailRef.current.value)) {
+      errors.email = 'This is not a valid email format!';
+      emailRef.current.focus();
+    }
+
+    if (emailRef.current.value.length === 0 || firstNameRef.current.value.trim() === '') {
+      errors.email = 'Please enter a message';
+      emailRef.current.focus();
+    }
+    if (textAreaRef.current.value.length === 0 || firstNameRef.current.value.trim() === '') {
+      errors.message = 'Please enter a message';
+      textAreaRef.current.focus();
+    }
+
+    return errors;
+  };
+
+  const [user, setUser] = useState({
+    first_name: '',
+    last_name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validate);
   };
 
   return (
@@ -15,20 +67,60 @@ const Contact = () => {
       <section className={styles.container}>
         <h2>Contact Me</h2>
         <p>Hi there, contact me to ask me about anything you have in mind.</p>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           <div className={styles.name}>
-            <Input type="text" name="First name" placeholder="Enter your first name" className={styles.inputContainer} id="first_name" />
-            <Input type="text" name="Last name" placeholder="Enter your last name" className={styles.inputContainer} id="last_name" />
+            <Input
+              type="text"
+              text="First name"
+              name="first_name"
+              placeholder="Enter your first name"
+              className={styles.inputContainer}
+              id="first_name"
+              value={user.first_name}
+              innerRef={firstNameRef}
+              onChange={handleChange}
+              errorMessage={errors.first_name}
+            />
+            <Input
+              type="text"
+              text="Last name"
+              name="last_name"
+              placeholder="Enter your last name"
+              className={styles.inputContainer}
+              id="last_name"
+              value={user.last_name}
+              innerRef={lastNameRef}
+              onChange={handleChange}
+              errorMessage={errors.last_name}
+            />
           </div>
-          <Input type="email" name="Email" placeholder="yourname@email.com" className={styles.inputContainer} id="email" />
-          <textarea
-            className={styles.textarea}
-            id="message"
-            placeholder="Send me a message and I&rsquo;ll reply to you as soon as possible...
-"
+          <Input
+            type="email"
+            text="Email"
+            name="email"
+            placeholder="yourname@email.com"
+            className={styles.inputContainer}
+            id="email"
+            value={user.email}
+            innerRef={emailRef}
+            onChange={handleChange}
+            errorMessage={errors.email}
           />
+          <div className={styles.textareaContainer}>
+            <textarea
+              className={styles.textarea}
+              name="message"
+              id="message"
+              ref={textAreaRef}
+              onChange={handleChange}
+              value={user.message}
+              placeholder="Send me a message and I&rsquo;ll reply to you as soon as possible...
+"
+            />
+            <small>{errors.message}</small>
+          </div>
           <div className={styles.conditions}>
-            <Input type="checkbox" className={`${styles.checkbox} ${isChecked ? styles.checked : ''}`} name="checkbox" checked={isChecked} onChange={handleCheckbox} id="checkbox" />
+            <Input type="checkbox" className={`${styles.checkbox} ${isChecked ? styles.checked : ''}`} text="checkbox" checked={isChecked} onChange={handleCheckbox} id="checkbox" />
             <span>You agree to providing your data to Victor Okoroji who may contact you.</span>
           </div>
           <button type="submit" className={styles.submitButton} id="btn__submit"> Send message </button>
